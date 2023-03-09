f609d685e7a19e73b252fad6fa20463c40e6d91a
LaSandoval <126043392+LaSandoval@users.noreply.github.com>
2023-03-08 20:39:11 -0500

    Add files via upload

---
+++ areas/Ppal.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.areas;
+public class Ppal {
+    public static void main(String[] args) {
+        Areas figura=new Areas();
+        figura.leerDouble();
+        figura.calcularAreaCirculo();
+        figura.calcularAreaCuadrado();
+        figura.imprimirResultado();
+    }
+    
+}
